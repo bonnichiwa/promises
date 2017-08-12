920776091d46c0b887b4f753db4014df5b53b382
bonnichiwa <bonnie.pham@ryerson.ca>
2017-08-13 00:57:42 +0400

    emails will now load on button click

--- promises.js
+++ promises.js
@@ -16,35 +16,33 @@ var EmailAccount = function() {
     inboxButton: document.getElementById('inbox-button'),
     outboxButton: document.getElementById('outbox-button'),
     spamButton: document.getElementById('spam-button')
+  };
 };
 
 /**
- * Initalize EmailAccount
+ * Initalize EmailAccount with inbox emails
  *
  * no @returns
  */
 EmailAccount.prototype.init = function() {
-  this.loadInbox().then(function(emails) {
-    this.inbox = emails;
-    console.log(this.inbox);
-  }.bind(this));
+  this.displayInboxEmails();
 };
 
 /**
- * EmailAccount's call to API simulation to load emails
+ * EmailAccount's call to API simulation to load inbox emails
  *
  * @returns {Array} 
  */
 EmailAccount.prototype.loadInbox = function() {
-    return new Promise(function(resolve, reject) {
+  return new Promise(function(resolve, reject) {
     setTimeout(function(){
       resolve(["Email about dogs", "Email about cats", "Emails about fishes"]);
-    }, 5000);
+    }, 1000);
   });
 };
 
 /**
- * EmailAccount's call to API simulation to load emails
+ * EmailAccount's call to API simulation to load outbox emails
  *
  * no @returns
  */
@@ -52,12 +50,12 @@ EmailAccount.prototype.loadOutbox = function() {
   return new Promise(function(resolve, reject) {
     setTimeout(function(){
       resolve(["Sending an email about the weather", "Sending an email about the colour of the sky", "Sending an email about how cold it is in Dubai"]);
-    }, 5000);
+    }, 1000);
   });
 };
 
 /**
- * EmailAccount's call to API simulation to load emails
+ * EmailAccount's call to API simulation to load spam emails
  *
  * no @returns
  */
@@ -65,33 +63,82 @@ EmailAccount.prototype.loadSpam = function() {
   return new Promise(function(resolve, reject) {
     setTimeout(function(){
       resolve(["Uninteresting email about things you don't care about", "Uninteresting email about things you used to care about", "Uninteresting email about things you never heard of"]);
-    }, 5000);
+    }, 1000);
   });
 };
 
+/** 
+ * Display inbox emails after loaded
+ *
+ * no @returns
+ */
+EmailAccount.prototype.displayInboxEmails = function() {
+  if (this.inbox.length > 0) {
+    this.htmlElement.emailContent.textContent = this.inbox;
+  } else {
+    this.loadInbox().then(function(emails) {
+      this.inbox = emails;
+      this.htmlElement.emailContent.textContent = this.inbox;
+    }.bind(this));
+  }
+} 
+
 /**
+ * Display outbox emails after loaded 
  *
+ * no @returns
+ */
+EmailAccount.prototype.displayOutboxEmails = function() {
+  if (this.outbox.length > 0) {
+    this.htmlElement.emailContent.textContent = this.outbox;
+  } else {
+    this.loadOutbox().then(function(emails) {
+      this.outbox = emails;
+      this.htmlElement.emailContent.textContent = this.outbox;
+    }.bind(this));
+  }
+} 
+
+/**
+ * Display spam emails after loaded
  *
+ * no @returns
+ */
+EmailAccount.prototype.displaySpamEmails = function() {
+  if (this.spam.length > 0) {
+    this.htmlElement.emailContent.textContent = this.spam;
+  } else {
+    this.loadSpam().then(function(emails) {
+      this.spam = emails;
+      this.htmlElement.emailContent.textContent = this.spam;
+    }.bind(this));
+  }
+} 
+
+/**
+ * Define the list of observers
+ *
+ * no @returns
  */
 EmailAccount.prototype.observers = function() {
   //When Inbox button has been clicked
-  this.htmlElement.inboxButton.addEventListener('click', this.onButtonClick(inbox));
+  if (this.htmlElement.inboxButton) {
+    this.htmlElement.inboxButton.addEventListener('click', this.displayInboxEmails.bind(this));
+  }
+
   //When Outbox button has been clicked
-  this.htmlElement.outboxButton.addEventListener('click', this.onButtonClick.bind(outbox));
+  if (this.htmlElement.outboxButton) {
+    this.htmlElement.outboxButton.addEventListener('click', this.displayOutboxEmails.bind(this));
+  }
+
   //When Spam button has been clicked
-  this.htmlElement.spamButton.addEventListener('click', this.onButtonClick.bind(spam));
+  if (this.htmlElement.spamButton) {
+    this.htmlElement.spamButton.addEventListener('click', this.displaySpamEmails.bind(this));
+  }
 }; 
 
-/**
- *
- *
- */
-EmailAccount.prototype.onButtonClick = function(type) {
-  if (type === inbox) {
-    this.loadInbox();
-  } else if (type === outbox) {
-    this.loadOutbox();
-  } else if (type === spam) {
-    this.loadSpam();
-  }
-};
+// Instantiate emailAccountComponent
+  window.emailAccountComponent = new EmailAccount();
+
+  emailAccountComponent.observers();
+  emailAccountComponent.init();
